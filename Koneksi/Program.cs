b535a647ba0d136c0a45d8cc9e28aa9c9b011ec1
Reyfadh07 <raihan.farmasi@gmail.com>
2022-03-16 14:19:04 +0700

    Add project files.

---
+++ Koneksi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using System;
+
+
+namespace Koneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            new Program().Connecting();
+        }
+        public void Connecting()
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=LAPTOP-GK6AO77T;database=ProdiTI;User ID=sa;Password=Rey071201")
+                )
+            {
+                con.Open();
+                Console.WriteLine("Berhasil terhubung ke database!");
+            }
+        }
+    }
+}
